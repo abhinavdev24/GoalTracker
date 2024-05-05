58e1e3556868471fe3e81c3fef36039d1929a794
Abhinav Kumar <1rn18cs002.abhinav@gmail.com>
2024-05-05 19:47:44 +0530

    feat: add and delete goals

---
+++ components/GoalItem.jsx
@@ -0,0 +1,29 @@
+import { Pressable, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
+
+const GoalItem = (props) => {
+  return (
+    <View style={styles.goalItem}>
+      <Pressable
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        android_ripple={{ color: "#dddddd" }}
+      >
+        <Text style={styles.goalText}>{props.text}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default GoalItem;
+
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  goalText: {
+    padding: 8,
+    color: "white",
+  },
+});
